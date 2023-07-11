7d9b580bbabbc36de5a89cb338482eee91c8ed0f
Savino Lugo <Savinolugo@gmail.com>
2023-07-11 11:10:09 -0500

    Building auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,43 @@
+import { useState, useEffect } from 'react'
+import { Route, Routes } from 'react-router'
+import { CheckSession } from './services/Auth'
+import Register from './pages/Register'
+import SignIn from './pages/SignIn'
 import './App.css'
 
 const App = () => {
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    // Check if token exists before requesting to validate the token
+    if (token) {
+      checkToken()
+    }
+  }, [])
+
+  const handleLogOut = () => {
+    //Reset all auth related state and clear localStorage
+    setUser(null)
+    localStorage.clear()
+  }
+
+  const checkToken = async () => {
+    //If a token exists, sends token to localStorage to persist logged in user
+    const user = await CheckSession()
+    setUser(user)
+  }
+
   return (
     <div className="App">
-      <h1></h1>
+      <Nav user={user} handleLogOut={handleLogOut} />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn setUser={setUser} />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </main>
     </div>
   )
 }
