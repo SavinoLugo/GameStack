55a58c4cb9c70927f3037d8da030092401eb2823
Savino Lugo <Savinolugo@gmail.com>
2023-07-12 10:17:52 -0500

    Auth working in Back end/Tested in insomnia

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,26 +4,25 @@ import { CheckSession } from './services/Auth'
 import Register from './pages/Register'
 import SignIn from './pages/SignIn'
 import './App.css'
+import Nav from './components/Nav'
+import Home from './pages/Home'
 
 const App = () => {
   const [user, setUser] = useState(null)
 
   useEffect(() => {
     const token = localStorage.getItem('token')
-    // Check if token exists before requesting to validate the token
     if (token) {
       checkToken()
     }
   }, [])
 
   const handleLogOut = () => {
-    //Reset all auth related state and clear localStorage
     setUser(null)
     localStorage.clear()
   }
 
   const checkToken = async () => {
-    //If a token exists, sends token to localStorage to persist logged in user
     const user = await CheckSession()
     setUser(user)
   }
